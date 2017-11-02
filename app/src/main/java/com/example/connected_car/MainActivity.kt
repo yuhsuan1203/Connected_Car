5e7ccb9d91a88f380d15ea6760a51e819c54da67
yuhsuan1203 <t22170000@gmail.com>
2017-11-02 17:40:23 +0800

    Upload Image to Firebase

--- app/src/main/java/com/example/connected_car/MainActivity.kt
+++ app/src/main/java/com/example/connected_car/MainActivity.kt
@@ -1,29 +1,71 @@
 package com.example.connected_car
 
 import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import java.io.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    internal val SELECT_FILE = 1
-    internal val REQUEST_CAMERA = 0
+    private val SELECT_FILE = 1
+    private val REQUEST_CAMERA = 0
     internal lateinit var userChoosenTask: String
     internal lateinit var imageSelected: ImageView
+    private var filePath: Uri? = null
+
+    internal var storage: FirebaseStorage? = null
+    internal var storageReference: StorageReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Init Firebase
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage!!.reference
+
         val btnSelect = findViewById(R.id.bSelectImage) as Button
         btnSelect.setOnClickListener{
             selectImage()
         }
         imageSelected = findViewById(R.id.imageToUpload) as ImageView
+        val btnUpload = findViewById(R.id.bUploadImage) as Button
+        btnUpload.setOnClickListener{
+            uploadImage()
+        }
+    }
+
+    private fun uploadImage() {
+        if(filePath != null)
+        {
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Uploading...")
+            progressDialog.show()
+
+            val imageRef = storageReference!!.child("images/" + UUID.randomUUID().toString())
+            imageRef.putFile(filePath!!)
+                    .addOnSuccessListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext, "Image Uploaded", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnProgressListener { taskSnapshot ->
+                        val progress = 100.0 * taskSnapshot.bytesTransferred/taskSnapshot.totalByteCount
+                        progressDialog.setMessage("Uploaded " + progress.toInt() + "%...")
+                    }
+        }
     }
 
     private fun selectImage() {
@@ -61,18 +103,17 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE)
     }
 
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (resultCode == RESULT_OK) {
+        if (resultCode == RESULT_OK && data != null && data.data != null) {
+            filePath = data.data
             if (requestCode == SELECT_FILE)
                 onSelectFromGalleryResult(data)
             else if (requestCode == REQUEST_CAMERA)
                 onCaptureImageResult(data)
         } else {
-            val builder = AlertDialog.Builder(this@MainActivity)
-            builder.setTitle("You don't select any images!")
-            builder.show()
+            Toast.makeText(applicationContext, "You don't select a image", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -81,7 +122,7 @@ class MainActivity : AppCompatActivity() {
         val bytes = ByteArrayOutputStream()
         thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes)
 
-        val destination = File(Environment.getExternalStorageDirectory(),
+        /*val destination = File(Environment.getExternalStorageDirectory(),
                 System.currentTimeMillis().toString() + ".jpg")
 
         val fo: FileOutputStream
@@ -94,7 +135,7 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         } catch (e: IOException) {
             e.printStackTrace()
-        }
+        }*/
 
         imageSelected.setImageBitmap(thumbnail)
     }
@@ -103,7 +144,7 @@ class MainActivity : AppCompatActivity() {
         if (data != null) {
             var bm: Bitmap? = null
             try {
-                bm = MediaStore.Images.Media.getBitmap(applicationContext.contentResolver, data.data)
+                bm = MediaStore.Images.Media.getBitmap(applicationContext.contentResolver, filePath)
             } catch (e: IOException) {
                 e.printStackTrace()
             }
